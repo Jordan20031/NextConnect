5431bb46f39a0e1ee8eeea74f4368097e70fdd75
rimworlds <ethan.capus@outlook.fr>
2023-06-19 15:26:16 +0200

    added a few function to modifiy user table and discution table

--- basededonnee/database.go
+++ basededonnee/database.go
@@ -5,6 +5,16 @@ type User struct {
 	Email    string
 	Password string
 	Role     string
+	Pdp      []byte
+}
+
+type discussions struct {
+    ID int64
+    image []byte
+	titre string
+    description string
+    nmbreDeLikes int64
+    idUser int64
 }
 
 func updateRole(db *sql.DB, user *User, newRole string) error {
@@ -23,23 +33,38 @@ func deleteUser(db *sql.DB, user *User) error {
 	return err
 }
 
-func insertUser(db *sql.DB, user *User) error {
-	insertUser := "INSERT INTO users (Username, Email, Password, Role) VALUES (?, ?, ?, ?)"
-	result, err := db.Exec(insertUser, user.Username, user.Email, user.Password, user.Role)
+func updateUsername(db *sql.DB, userID int64, newUsername string) error {
+	updateQuery := "UPDATE users SET Username = ? WHERE ID = ?"
+	_, err := db.Exec(updateQuery, newUsername, user.ID)
 	if err != nil {
 		return err
 	}
-	user.ID, _ = result.LastInsertId()
+	user.Username = newUsername
 	return nil
 }
 
 func insertUser(db *sql.DB, user *User) error {
-	insertUser := "INSERT INTO users (Username, Email, Password, Role) VALUES (?, ?, ?, ?)"
-	result, err := db.Exec(insertUser, user.Username, user.Email, user.Password, user.Role)
+	// Vérification de l'existence d'un utilisateur avec le même nom d'utilisateur ou la même adresse e-mail
+	existsQuery := "SELECT COUNT(*) FROM users WHERE Username = ? OR Email = ?"
+	var count int
+	err := db.QueryRow(existsQuery, user.Username, user.Email).Scan(&count)
+	if err != nil {
+		return err
+	}
+
+	if count > 0 {
+		// Un utilisateur avec le même nom d'utilisateur ou la même adresse e-mail existe déjà
+		return fmt.Errorf("un utilisateur avec le même nom d'utilisateur ou la même adresse e-mail existe déjà")
+	}
+
+	// Insertion de l'utilisateur dans la base de données
+	insertQuery := "INSERT INTO users (Username, Email, Password, Role, pdp) VALUES (?, ?, ?, ?, ?)"
+	result, err := db.Exec(insertQuery, user.Username, user.Email, user.Password, user.Role, user.Pdp)
 	if err != nil {
 		return err
 	}
 	user.ID, _ = result.LastInsertId()
+
 	return nil
 }
 
@@ -53,3 +78,35 @@ func updatePassword(db *sql.DB, user *User, newPassword string) error {
 	return nil
 }
 
+func getUserByUsername(db *sql.DB, username string) (User, error) {
+	query := "SELECT ID, Username, Email, Password, Role, pdp FROM users WHERE Username = ?"
+
+	var user User
+	err := db.QueryRow(query, username).Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.Role, &user.Pdp)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+func insertDiscussion(db *sql.DB, discussion *Discussions) error {
+	insertQuery := "INSERT INTO discussions (image, titre, description, nmbreDeLikes, idUser) VALUES (?, ?, ?, ?, ?)"
+	result, err := db.Exec(insertQuery, discussion.Image, discussion.Titre, discussion.Description, discussion.NmbreDeLikes, discussion.IDUser)
+	if err != nil {
+		return err
+	}
+	discussion.ID, _ = result.LastInsertId()
+	return nil
+}
+
+func getDiscussionByID(db *sql.DB, discussionID int64) (Discussions, error) {
+	query := "SELECT ID, image, titre, description, nmbreDeLikes, idUser FROM discussions WHERE ID = ?"
+
+	var discussion Discussions
+	err := db.QueryRow(query, discussionID).Scan(&discussion.ID, &discussion.Image, &discussion.Titre, &discussion.Description, &discussion.NmbreDeLikes, &discussion.IDUser)
+	if err != nil {
+		return Discussions{}, err
+	}
+
+	return discussion, nil
+}
